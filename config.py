b6eff7d28a84fe53851c15dc63391cd85be96499
Silver2749 <7shanebraganza@gmail.com>
2026-01-28 02:17:11 +0530

    .

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-import os
+import os 
 
 class Config:
     SECRET_KEY = "secret-key"
     SQLALCHEMY_DATABASE_URI = "sqlite:///./db.sqlite3"
     JWT_SECRET_KEY = "jwt-secret"
+#need to learn authoriza stat
